d00427e0cc511db9f818f6e3f9dbf058b9cf3f95
BerishaHysen <hb46326@ubt-uni.net>
2022-05-31 21:06:11 +0200

    creating eureka server and adding eureka client to microservices

---
+++ discovery-server/src/test/java/com/hysenberisha/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hysenberisha.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
